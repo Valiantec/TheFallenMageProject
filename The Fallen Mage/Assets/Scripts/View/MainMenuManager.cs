ae082ca634393e086a1822c5f42b7fd01dca4f22
Osama4499 <osalzoubi@gmail.com>
2020-12-25 21:50:43 +0200

    Initial upload

---
+++ The Fallen Mage/Assets/Scripts/View/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void NewGameBtn()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+}
